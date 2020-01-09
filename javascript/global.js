9a2f4568fd2f4f62508690e08bf3d6de4bcb18a5
Oxisnet <41337924+moaidesign@users.noreply.github.com>
2020-01-09 16:10:53 +0100

    pre upini

--- javascript/global.js
+++ javascript/global.js
@@ -1,8 +1,1 @@
-$("#re-contact").submit(function(e) {
-  e.preventDefault();
 
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Grazie ti ricontatteremo il prima possibile!");
-  });
-});
